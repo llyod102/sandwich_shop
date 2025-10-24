8c973fc206b3f8bba01de97ec679b705c9cb6ae0
Josh Ibeji <joshuaibeji332@gmail.com>
2025-10-24 15:14:52 +0100

    Source Control make view changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,53 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(const App());
-  
 }
-class App extends StatelessWidget{
-   const App({super.key});
 
-   @override
-   Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'Sandwich Shop App',
-      
-
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Sandwich Counter')),
-        body: const Center(
-        child: OrderItemDisplay(5,'Club',3,2,'BLT','Veggie'),
-          ),
-      ),
-      
-    );
-
-
-
-
-   }
-   
-}
-class OrderItemDisplay extends StatelessWidget {
-  final String itemType;
-  final String itemType2;
-  final String itemType3;
-  final int clubQuantity;
-  final int bltQuantity;
-  final int veggieQuantity;
-
-  const OrderItemDisplay(this.clubQuantity, this.itemType, this.bltQuantity,this.veggieQuantity, this.itemType2,this.itemType3,{super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return Container(
-    color: Colors.blue,
-    width: 800,
-    height: 300,
-    alignment: Alignment.topLeft,
-
-    child:  Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return MaterialApp(
+      title: 'Sandwich Shop App',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Sandwich Counter')),
+        body: Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Flexible(child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text('$clubQuantity $itemType  sandwich(es): ${'🥪' * clubQuantity}',
-          softWrap: true,
+        const OrderItemDisplay(5, 'Footlong'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => print('Add button pressed!'),
+              child: const Text('Add'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Remove button pressed!'),
+              child: const Text('Remove'),
+            ),
+          ],
+        ),
+      ],
+    ),
+  ),
+      ));
 
-     
+  }
+}
 
-        ))),
-         Flexible(child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text('$bltQuantity $itemType2 sandwich(es): ${'🥪' * bltQuantity}',
-          softWrap: true,
+class OrderItemDisplay extends StatelessWidget {
+  final int quantity;
+  final String itemType;
 
-        ))),
-        Flexible(child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text('$veggieQuantity $itemType3 sandwich(es): ${'🥪' * veggieQuantity}',
+  const OrderItemDisplay(this.quantity, this.itemType, {super.key});
 
-          softWrap: true,
-
-        ))),
-
-
-
-
-
-      ]
-
-   ));
+  @override
+  Widget build(BuildContext context) {
+    return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
   }
 }
 
